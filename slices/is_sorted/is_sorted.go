1099699c68e49ce21df77efc079239f232cd9d9b
Oleksandr Mykhailov <alex.mykhailove@gmail.com>
2023-08-21 18:46:57 +0300

    feat: add IsSorted function

---
+++ slices/is_sorted/is_sorted.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+// IsSorted reports whether x is sorted in ascending order.
+func main() {
+	fmt.Println("slices.IsSorted([]int{1, 2, 3}) ==", slices.IsSorted([]int{1, 2, 3}))
+	fmt.Println("slices.IsSorted([]string{\"C\", \"B\", \"A\"}) ==", slices.IsSorted([]string{"C", "B", "A"}))
+}

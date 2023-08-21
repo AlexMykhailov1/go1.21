791228f4f4255a039517b4f1342ac465314aa3a8
Oleksandr Mykhailov <alex.mykhailove@gmail.com>
2023-08-21 18:48:28 +0300

    chore: add comment for IsSortedFunc

--- slices/is_sorted/is_sorted.go
+++ slices/is_sorted/is_sorted.go
@@ -10,3 +10,5 @@ func main() {
 	fmt.Println("slices.IsSorted([]int{1, 2, 3}) ==", slices.IsSorted([]int{1, 2, 3}))
 	fmt.Println("slices.IsSorted([]string{\"C\", \"B\", \"A\"}) ==", slices.IsSorted([]string{"C", "B", "A"}))
 }
+
+// You can use IsSortedFunc to understand if your slice is sorted by your own conditions.
